0bdbfa7b3a750638fb0290eb5fb164c7a50c115e
RoshanKolhe <kolheroshan508@gmail.com>
2025-05-12 14:36:53 +0530

    added  new sections in product details

--- src/sections/our-project-details/view/our-project-details-view.js
+++ src/sections/our-project-details/view/our-project-details-view.js
@@ -1,6 +1,9 @@
 import { useScroll } from 'framer-motion';
 // components
 import ScrollProgress from 'src/components/scroll-progress';
+import HomeStories from 'src/components/home-stories/HomeStories';
+import AwardsSection from 'src/components/award-section/AwardsSection';
+import ContactConsultationForm from 'src/components/contact-consultation-form/ContactConsultationForm';
 import OurProjectHero from '../our-project-details-hero';
 
 // ----------------------------------------------------------------------
@@ -13,8 +16,9 @@ export default function OurProjectDetailsView() {
       <ScrollProgress scrollYProgress={scrollYProgress} />
 
       <OurProjectHero />
-
-     
+      <AwardsSection />
+      <HomeStories />
+      <ContactConsultationForm />
     </>
   );
 }
