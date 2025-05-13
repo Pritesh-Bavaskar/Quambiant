5fc9b5db3fe85472b33a7a22b0b3c0b820125e1c
Pritesh-Bavaskar <priteshbavaskar@gmail.com>
2025-05-13 20:45:58 +0530

    Amaranthine mobile changes fixed

--- src/sections/home/view/home-view.js
+++ src/sections/home/view/home-view.js
@@ -6,8 +6,8 @@ import HomeStories from 'src/components/home-stories/HomeStories';
 import SpotlightCarousel from 'src/sections/home/SpotlightCarousel';
 import BuildGreenSection from 'src/sections/home/BuildGreenSection';
 import AwardsSection from 'src/components/award-section/AwardsSection';
-import AmaranthineGrid from 'src/components/section-into/AmaranthineGrid';
-import AmaranthineCard from 'src/components/section-into/AmaranthineCard';
+import AmaranthineGrid from 'src/components/section-amaranthine/AmaranthineGrid';
+import AmaranthineCard from 'src/components/section-amaranthine/AmaranthineCard';
 import ConceptToConcreteSection from 'src/sections/home/ConceptToConcreteSection';
 import ContactConsultationForm from 'src/components/contact-consultation-form/ContactConsultationForm';
 import UpcomingLaunchesCarousel from 'src/components/upcoming-and-latest-launches/UpcomingLaunchesCarousel';
@@ -15,7 +15,7 @@ import EmiCalculator from 'src/components/emi-calculator/EmiCalculator';
 import IntroSection from 'src/sections/home/IntroSection';
 import HomeHero from '../home-hero';
 import CountUpSection from '../../../components/count-up/CountUp';
-import { SectionIntro } from '../../../components/section-into/SectionIntro';
+import { SectionAmaranthine } from '../../../components/section-amaranthine/SectionAmaranthine';
 // ----------------------------------------------------------------------
 
 export default function HomeView() {
@@ -28,7 +28,7 @@ export default function HomeView() {
       <IntroSection />
       <HomeHero />
       <CountUpSection />
-      <SectionIntro />
+      <SectionAmaranthine />
       {/* <AmaranthineGrid /> */}
       {/* <AmaranthineCard /> */}
       <ConceptToConcreteSection />
